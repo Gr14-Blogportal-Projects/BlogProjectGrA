920a4f201f14b1a91bb65a4fbb3080709c5f6c3e
Alexander Jonsson <alexander__jm@hotmail.com>
2022-08-30 16:36:20 +0200

    Small visuals fixes + selecting tags in creating post (but it doesn't save)

--- BlogProjectGrA/Controllers/PostController.cs
+++ BlogProjectGrA/Controllers/PostController.cs
@@ -45,7 +45,7 @@ namespace BlogProjectGrA.Controllers
         // GET: HomeController1/Create
         public ActionResult Create(int id, int blogId, int tagid)
         {
-            
+            var tag = _tagService.GetTags(); //new from 30/aug
             var user = _userManager.GetUserAsync(User).Result;
 
             if (user.Blogs.Count <= 0) 
@@ -54,7 +54,7 @@ namespace BlogProjectGrA.Controllers
                 return RedirectToAction("Create", "Blog");
             }
             ViewBag.BlogId = new SelectList(user.Blogs, "Id", "Title" );
-            ViewBag.TagId = new SelectList(user.Tags, "Id", "Title");
+            ViewBag.TagId = new SelectList(tag.Select(t => t.Name), "Tags" ); //new from 30/aug
             var post = new Post();
             
             return View(post);
@@ -65,12 +65,13 @@ namespace BlogProjectGrA.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Post post, int blogId, int tagid)
         {
+            var tag = _tagService.GetTags(); //new from 30/aug
             var blog = _blogService.GetBlog(blogId);
             post.Blog = blog;
             _postService.CreatePost(post);
             var user = _userManager.GetUserAsync(User).Result;
             ViewBag.BlogId = new SelectList(user.Blogs, "Id", "Title");
-            ViewBag.TagId = new SelectList(user.Tags, "Id", "Title");
+            ViewBag.TagId = new SelectList(tag.Select(t => t.Name), "Tags"); //new from 30/aug
             //return RedirectToAction(nameof(Index));
             return RedirectToAction("Index", "BrowseBlog"); //TODO Redirect to the blog where you make the post
         }
