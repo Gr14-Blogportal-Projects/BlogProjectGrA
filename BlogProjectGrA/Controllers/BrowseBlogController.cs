f7c787bfa42cf5b9cc5fe62ee8c8b72b97ff63c0
zainab mazari <mazarizainab@gmail.com>
2022-09-01 16:10:36 +0200

    fixed post by Date

--- BlogProjectGrA/Controllers/BrowseBlogController.cs
+++ BlogProjectGrA/Controllers/BrowseBlogController.cs
@@ -5,7 +5,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using BlogProjectGrA.ViewModels;
+using BlogProjectGrA.Models.ViewModels;
+
 
 namespace BlogProjectGrA.Controllers
 {
@@ -17,6 +18,7 @@ namespace BlogProjectGrA.Controllers
 
         private readonly IPostService _postService;
         private readonly ITagService _tagService;
+      
 
         public BrowseBlogController(IBlogService blogService, UserManager<User> userManager, IPostService postService ,ITagService tagService)
         {
@@ -55,10 +57,43 @@ namespace BlogProjectGrA.Controllers
             return View(vm);
         }
 
-        public ActionResult Details(int id)
+        public ActionResult Details(int id, int year, int month)
         {
             var blog = _blogService.GetBlog(id);
-            return View(blog);
+            List<Post> posts;
+            if (year != 0 || month != 0)
+            {
+                posts = _postService.GetPostByDate(id, year, month).ToList();
+            }
+            else
+            {
+                posts = blog.Posts.ToList();
+            }
+            var allDates = blog.Posts.OrderByDescending(p=>p.CreateAt).ToList().Select(p => p.CreateAt).ToList();
+
+            var dates = new List<DatesCountVM>();
+            foreach (var item in allDates)
+            {
+                var existing = dates.FirstOrDefault(d => d.Date.Year == item.Year && d.Date.Month == item.Month);
+                if (existing == null)
+                {
+                    var count = allDates.Where(d => d.Date.Year == item.Year && d.Date.Month == item.Month).Count();
+                    dates.Add(new DatesCountVM { Count = count, Date = item});
+                }
+            }
+
+            var dateByPostVm = new DateByPostVM()
+            {
+                Blog = blog,
+                Posts = posts,
+                Dates = dates,
+                Year = year,    
+                Month=month
+            };
+
+            return View(dateByPostVm);
+            //var blog = _blogService.GetBlog(id);
+            //return View(blog);
         }
 
         public ActionResult Blog(string id)
