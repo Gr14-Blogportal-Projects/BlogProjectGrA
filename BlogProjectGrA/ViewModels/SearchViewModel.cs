49ff86c3aac22393ddb9b7a1aff3e1b666ca2ba0
Alexander Jonsson <alexander__jm@hotmail.com>
2022-09-01 16:48:58 +0200

    search function backend complete

---
+++ BlogProjectGrA/ViewModels/SearchViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BlogProjectGrA.Models;
+
+namespace BlogProjectGrA.ViewModels
+{
+    public class SearchViewModel
+    {
+
+        public IEnumerable<Blog> Blogs { get; set; }
+
+        public IEnumerable<Post> Posts { get; set; }
+    }
+}
