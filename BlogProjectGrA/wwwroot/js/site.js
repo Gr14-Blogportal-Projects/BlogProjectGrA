c24f3588eb8f4af526bb46e9fe491e598bb8226f
Gayatri Prajapati <999mails@gmail.com>
2022-09-13 12:25:26 +0100

    done java script page

--- BlogProjectGrA/wwwroot/js/site.js
+++ BlogProjectGrA/wwwroot/js/site.js
@@ -5,8 +5,8 @@
 
 
 <script>
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
       return new bootstrap.Tooltip(tooltipTriggerEl)
     })
    popover-bg:  violet;
